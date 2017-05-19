c21bc98b08b94496d70522e0169e799afd07b487
yollock <yollock@163.com>
2017-05-19 15:19:21 +0800

    添加清除缓存的注解 - @CacheExpire, 如果方法标注了此注解, 调用时会触发清除缓存数据

---
+++ src/main/java/com/sedis/cache/annotation/CacheExpire.java
@@ -0,0 +1,15 @@
+package com.sedis.cache.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CacheExpire {
+    // 缓存key
+    String key() default "";
+}
